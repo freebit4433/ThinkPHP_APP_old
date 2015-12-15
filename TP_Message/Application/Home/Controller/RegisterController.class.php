25e51533b8133c643c24ddd070dd6991502e1e00
freebit4433 <freebit4433@sina.cn>
2015-12-15 12:50:03 +0800

    RegisterController over
    完成RegisterController，及它的模板

--- TP_Message/Application/Home/Controller/RegisterController.class.php
+++ TP_Message/Application/Home/Controller/RegisterController.class.php
@@ -11,8 +11,9 @@ class RegisterController extends Controller{
 			$this->error('拒绝访问!','../Login/index',1);
 		}else{
 			$userTable = D('Register');
-			if($data = $userTable->create()){
-				dump($data);
+			if($data = $userTable->field('username,password,registertime,imgnum,sex,webpage')->create()){
+				$userTable->add();
+				$this->success('注册成功！','../Login/index');
 			}else{
 				$this->error($userTable->getError(),'index',1);
 			}
--- TP_Message/Application/Home/Model/RegisterModel.class.php
+++ TP_Message/Application/Home/Model/RegisterModel.class.php
@@ -6,7 +6,14 @@ class RegisterModel extends Model{
 	protected $tableName = 'table2user';
 	protected $_validate = array(
 		array('username','require','请输入用户名！'),
-		
+		array('username','','该用户名已被占用，请重新输入！',0,'unique'),
+		array('password','require','请输入密码！'),
+		array('passwordconfirm','password','确认密码输入不一致！',0,'confirm'),
+		//array('webpage','url','请输入正确格式的URL！')
+	);
+	protected $_auto = array(
+		array('password','md5',1,'function'),
+		array('registertime','time',1,'function'),
 	);
 }
 
